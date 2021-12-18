f5fd2afc098f45903a1169da083ab421ea9a7e2c
BehCS-slearn-xbt <behcsiang@gmail.com>
2021-12-19 03:25:16 +0800

    update pom and extent report

--- src/test/java/utils/ExtentManager.java
+++ src/test/java/utils/ExtentManager.java
@@ -8,7 +8,7 @@ public class ExtentManager {
 	public static final ExtentReports extentReports = new ExtentReports();
 	
     public synchronized static ExtentReports createExtentReports() {
-        ExtentSparkReporter reporter = new ExtentSparkReporter("./extent-reports/extent-report.html");
+        ExtentSparkReporter reporter = new ExtentSparkReporter("./test-output/extent-reports/extent-report.html");
         reporter.config().setReportName("Medicare Test Extent Report");
         extentReports.attachReporter(reporter);
         extentReports.setSystemInfo("Project", "Simplilearn Capstone");
